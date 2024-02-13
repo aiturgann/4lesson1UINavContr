df41ac9b1b2c369730cfc560f913b08eb97ebd53
aiturann <aiturgannnnnn@gmail.com>
2024-02-13 17:48:09 +0600

    lesson 1

---
+++ 4lesson1UINavContr/Resources/Color.swift
@@ -0,0 +1,8 @@
+//
+//  Color.swift
+//  4lesson1UINavContr
+//
+//  Created by Aiturgan Kurmanbekova on 10/2/24.
+//
+
+import Foundation
---
+++ 4lesson1UINavContr/Screens/SuccessVC.swift
@@ -0,0 +1,35 @@
+//
+//  SuccessViewController.swift
+//  4lesson1UINavContr
+//
+//  Created by Aiturgan Kurmanbekova on 13/2/24.
+//
+
+import UIKit
+
+class SuccessVC: UIViewController {
+    
+    private let successImage: UIImageView = {
+        let successImage = UIImageView()
+        successImage.image = .success
+        successImage.translatesAutoresizingMaskIntoConstraints = false
+        return successImage
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(hex: "#048494")
+        setupUI()
+
+    }
+    
+    private func setupUI() {
+        view.addSubview(successImage)
+        NSLayoutConstraint.activate(
+            [successImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+             successImage.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+             successImage.heightAnchor.constraint(equalToConstant: 235),
+             successImage.widthAnchor.constraint(equalToConstant: 183.5)
+    ])
+    }
+}
